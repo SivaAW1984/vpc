7fe0fa3c5dabf8c1e341933d4adf4fb12e22d9ed
sivashankar84 <66830181+sivashankar84@users.noreply.github.com>
2024-04-16 13:48:19 +0100

    Update main.tf

--- Terraform-VPC/modules/main.tf
+++ Terraform-VPC/modules/main.tf
@@ -1,3 +1,30 @@
+provider "aws" {
+  region = local.region
+}
+
+resource "aws_vpc_ipam_pool" "this" {
+  description                       = "IPv4 pool"
+  address_family                    = "ipv4"
+  ipam_scope_id                     = aws_vpc_ipam.this.private_default_scope_id
+  locale                            = local.region
+  allocation_default_netmask_length = 16
+
+  tags = local.tags
+}
+
+resource "aws_vpc_ipam_pool_cidr" "this" {
+  ipam_pool_id = aws_vpc_ipam_pool.this.id
+  cidr         = "10.0.0.0/8"
+}
+
+resource "aws_vpc_ipam_preview_next_cidr" "this" {
+  ipam_pool_id = aws_vpc_ipam_pool.this.id
+
+  depends_on = [
+    aws_vpc_ipam_pool_cidr.this
+  ]
+}
+
 resource "aws_vpc" "vpc" {
   cidr_block           = "${var.vpc_cidr}"
   enable_dns_hostnames = true

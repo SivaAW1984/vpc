c8dbf5255d76bb61883d045bdc6f8b9b20909bc9
sivashankar84 <66830181+sivashankar84@users.noreply.github.com>
2024-04-23 16:38:10 +0100

    Update main.tf

--- Terraform-VPC/modules/main.tf
+++ Terraform-VPC/modules/main.tf
@@ -1,13 +1,20 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
- 
-  bucket = "my-siva-bucket"
-  acl    = "private"
- 
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
- 
-  versioning = {
-    enabled = true
-  }
+module "aft" {
+  source = "git@github.com:SivaAW1984/terraform-aws-control_tower_account_factory.git"
+
+  # Required Parameters
+  ct_management_account_id    = "953758143890"
+  log_archive_account_id      = "381492256674"
+  audit_account_id            = "975049967591"
+  aft_management_account_id   = "975049967591"
+  ct_home_region              = "us-east-1"
+  tf_backend_secondary_region = "us-west-2"
+
+  # Optional Parameters
+  terraform_distribution = "oss"
+  vcs_provider           = "codecommit"
+
+  # Optional Feature Flags
+  aft_feature_delete_default_vpcs_enabled = false
+  aft_feature_cloudtrail_data_events      = false
+  aft_feature_enterprise_support          = false
 }

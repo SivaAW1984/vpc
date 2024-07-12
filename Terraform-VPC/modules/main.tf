c84af58179be9feadf895af2905cc11b83d48817
sivashankar84 <66830181+sivashankar84@users.noreply.github.com>
2024-07-12 14:37:43 +0100

    Update main.tf

--- Terraform-VPC/modules/main.tf
+++ Terraform-VPC/modules/main.tf
@@ -1,20 +1,16 @@
-module "aft" {
-  source = "git@github.com:SivaAW1984/terraform-aws-control_tower_account_factory.git"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
 
-  # Required Parameters
-  ct_management_account_id    = "953758143890"
-  log_archive_account_id      = "381492256674"
-  audit_account_id            = "975049967591"
-  aft_management_account_id   = "975049967591"
-  ct_home_region              = "us-east-1"
-  tf_backend_secondary_region = "us-west-2"
-
-  # Optional Parameters
-  terraform_distribution = "oss"
-  vcs_provider           = "codecommit"
-
-  # Optional Feature Flags
-  aft_feature_delete_default_vpcs_enabled = false
-  aft_feature_cloudtrail_data_events      = false
-  aft_feature_enterprise_support          = false
+provider "azurerm" {
+  features {}
 }

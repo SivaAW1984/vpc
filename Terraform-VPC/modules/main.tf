98d9bbd2f36d942f9162b22a0e26290047e7c28a
sivashankar84 <66830181+sivashankar84@users.noreply.github.com>
2024-07-12 20:30:02 +0100

    Update main.tf

--- Terraform-VPC/modules/main.tf
+++ Terraform-VPC/modules/main.tf
@@ -1,8 +1,8 @@
-resource "random_pet" "rg_name" {
+resource "POC" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
 
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = random_pet.rg_name.id
+  name     = POC.rg_name.id
 }
